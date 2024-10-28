4068a2e015f9019456e4a1a35cbaf788c3d7f2d1
Factobot <alessandro.abreu.junior@hotmail.com>
2024-10-28 20:46:09 +0000

    latest

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,6 +112,20 @@
 					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
 				});
 
+				$('#_two').poptrox({
+					caption: function($a) { return $a.next('h3').text(); },
+					overlayColor: '#2c2c2c',
+					overlayOpacity: 0.85,
+					popupCloserText: '',
+					popupLoaderText: '',
+					selector: '.work-item a.image',
+					usePopupCaption: true,
+					usePopupDefaultStyling: false,
+					usePopupEasyClose: false,
+					usePopupNav: true,
+					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
+				});
+
 			});
 
 })(jQuery);
